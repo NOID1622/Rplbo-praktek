cf50025ff5ac6bbe922c4d1403c174abe3682cb8
NOID1622 <112671264+NOID1622@users.noreply.github.com>
2024-02-27 15:24:36 +0700

    Update Ninja.java

--- Ninja.java
+++ Ninja.java
@@ -1,10 +1,37 @@
 package com.ug7.soal2;
 
-public class Ninja class extend Player{
+public class Ninja extends Karakter{
     private boolean isAbilityReady;
-    public Ninja(String username,int level,int money,int HP, int weapon, boolean isWalk, boolean isAbilityReady){
-        super(username,level,money,HP,weapon,isWalk);
+    public Ninja(String username, String password) {
+        super(username, password);
+
+    
+
+    public Ninja(String username, int level, int money, int HP, int weapon, boolean isWalk, boolean isAbilityReady) {
+        super(username, level, money, HP, weapon, isWalk);
         this.isAbilityReady = isAbilityReady;
+    }
+    public void abilityAttack(Player Karakter) {
+        if (isAbilityReady && Karakter.() < 50) {
+            int damage = Karakter.();
+            Karakter.(damage);
+        }
+    }
+
+    public void normalAttack(Player Karakter) {
+        if (isWalk) {
+            int damage = Karakter.getHP();
+            Karakter.reduceHP(damage);
+        } else {
+            int damage = Karakter.getHP() / 2;
+            Karakter.reduceHP(damage);
+        }
+    }
+    public void walk(boolean type) {
+        super.walk(type);
+        if (type) {
+            System.out.println("Menghilang");
+        }
     }
 
 }
